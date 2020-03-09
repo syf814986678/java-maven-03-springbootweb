d64346c19167cfcb717ce3633b9c1c709abc00e2
syf <1@noahsark1.vip>
2020-03-09 14:16:00 +0800

    update测试

---
+++ src/main/java/com/shiyfian/controller/EmployeeController.java
@@ -0,0 +1,43 @@
+package com.shiyfian.controller;
+
+import com.shiyfian.Dao.DepartmentDao;
+import com.shiyfian.Dao.EmployeeDao;
+import com.shiyfian.pojo.Department;
+import com.shiyfian.pojo.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@Controller
+public class EmployeeController {
+    @Autowired
+    EmployeeDao employeeDao;
+    @Autowired
+    DepartmentDao departmentDao;
+    @RequestMapping("/employees")
+    public String list(Model model){
+        Collection<Employee> employees = employeeDao.getAll();
+        model.addAttribute("emps",employees);
+        return "employees/employees";
+    }
+    @GetMapping("/addemp")
+    public String toaddempspages(Model model){
+        //查出所有部门信息
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("department",departments);
+        return "employees/add";
+    }
+    @PostMapping("/addemp")
+    public String addemployees(Employee employee){
+        employeeDao.addEmployee(employee);//保存员工信息
+        System.out.println("save=>"+employee);
+        //添加操作
+        return "redirect:/employees";
+
+    }
+}
